0ea89faf35c3f39552c68b778669e60466d25a1c
earlgray <earlgray283@gmail.com>
2020-10-28 23:13:51 +0900

    add rust-toolchain

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,7 @@
 use num::{abs, integer::gcd};
 use permutohedron::LexicalPermutation;
 use proconio::{fastout, input, marker::*};
-use std::{
-    cmp,
-    collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque},
-    mem::swap,
-};
+use std::{cmp, collections::{BTreeMap, BTreeSet, BinaryHeap, HashMap, HashSet, VecDeque}, mem::swap};
 use text_io::{read, scan};
 const INF: i32 = std::i32::MAX;
 const MOD: i32 = 1e9 as i32 + 7;
@@ -22,7 +18,6 @@ const No: &str = "No";
 static dx: [i32; 4] = [1, 0, -1, 0];
 static dy: [i32; 4] = [0, -1, 0, 1];
 
-#[fastout]
 fn main() {
     
 }
