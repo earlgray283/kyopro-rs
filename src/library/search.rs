d91ae3c6eacffd0c7b1fb1533b12ca6daa396d17
earlgray <earlgray283@gmail.com>
2021-01-03 16:22:59 +0900

    bfs を追加

--- src/library/search.rs
+++ src/library/search.rs
@@ -1,89 +1,59 @@
-struct Search {
-    goal: (usize, usize),
+struct Search<T> {
     memo: Vec<Vec<i32>>,
     h: usize,
     w: usize,
-    c: Vec<Vec<char>>,
+    c: Vec<Vec<T>>,
 }
 
-impl Search {
-    fn new(h: usize, w: usize, c: Vec<Vec<char>>, goal: (usize, usize)) -> Search {
+impl<T> Search<T> {
+    fn new(h: usize, w: usize, c: Vec<Vec<T>>) -> Search<T> {
         Search {
             h,
             w,
             c,
-            goal,
             memo: vec![vec![-1; w]; h],
         }
     }
 
-    fn bfs(&mut self, start: (usize, usize)) {
-        let dx: [i32; 4] = [1, 0, -1, 0];
-        let dy: [i32; 4] = [0, -1, 0, 1];
+    fn init(&mut self) {
+        self.memo = vec![vec![-1; self.w]; self.h];
+    }
 
+    fn bfs1(&mut self, start: (usize, usize), f: fn(&Vec<Vec<T>>, (usize, usize)) -> bool) -> i32 {
+        let dxy = [(1, 0), (0, -1), (-1, 0), (0, 1)];
+        let mut ans = 0;
         let mut queue = VecDeque::new();
-        let mut x = 0;
-        self.memo[start.0][start.1] = x;
-
         queue.push_back(start);
+
+        self.memo[start.1][start.0] = 0;
+
         while !queue.is_empty() {
+            let now = queue.pop_front().unwrap();
             for i in 0..4 {
+                let res = now.add(dxy[i]);
+                if res.is_none() {
+                    continue;
+                }
 
+                let next = res.unwrap();
+                if next.0 >= self.w || next.1 >= self.h {
+                    continue;
+                }
+
+                if !f(&self.c, next) {
+                    continue;
+                }
+
+                if self.memo[next.1][next.0] != -1 {
+                    continue;
+                }
+
+                queue.push_back(next);
+                self.memo[next.1][next.0] = self.memo[now.1][now.0] + 1;
+                ans = ans.max(self.memo[next.1][next.0]);
             }
         }
-    }
 
-    #[allow(dead_code)]
-    fn dfs(&mut self, now: (usize, usize), x: i32) {
-        let dx: [i32; 4] = [1, 0, -1, 0];
-        let dy: [i32; 4] = [0, -1, 0, 1];
-
-        self.memo[now.0][now.1] = x;
-
-        for i in 0..4 {
-            let mut next = now;
-            match dy[i].is_negative() {
-                true => {
-                    if dy[i].abs() > 0 && next.0 == 0 {
-                        continue;
-                    }
-                    next.0 = next.0 - dy[i].abs() as usize;
-                }
-                false => {
-                    if next.0 + dy[i].abs() as usize >= self.h {
-                        continue;
-                    }
-                    next.0 = next.0 + dy[i].abs() as usize;
-                }
-            };
-            match dx[i].is_negative() {
-                true => {
-                    if dx[i].abs() > 0 && next.1 == 0 {
-                        continue;
-                    }
-                    next.1 = next.1 - dx[i].abs() as usize;
-                }
-                false => {
-                    if next.1 + dx[i].abs() as usize >= self.w {
-                        continue;
-                    }
-                    next.1 = next.1 + dx[i].abs() as usize;
-                }
-            };
-
-            if self.c[next.0][next.1] == '#' {
-                continue;
-            }
-
-            if self.memo[next.0][next.1] != -1 && self.memo[next.0][next.1] < x {
-                continue;
-            }
-
-            self.dfs(next, x + 1);
-        }
-    }
-
-    fn checker(target: usize, x: usize) -> bool {
-
+        ans
     }
 }
