650129d8c8f4de412e39e07ced05f0cc1ad0ef58
earlgray <earlgray283@gmail.com>
2022-05-09 14:13:15 +0900

    feat: sieve, prime_list

--- src/library.rs
+++ src/library.rs
@@ -1,2 +1,3 @@
 pub mod math;
 pub mod unionfind;
+pub mod prime;
---
+++ src/library/prime.rs
@@ -0,0 +1,58 @@
+pub struct Sieve {
+    table: Vec<bool>,
+}
+
+impl Sieve {
+    pub fn new(n: usize) -> Self {
+        let mut table = vec![true; n + 1];
+        table[0] = false;
+        table[1] = false;
+        for i in 2..=n {
+            if !table[i] {
+                continue;
+            }
+            let mut j = i * 2;
+            while j <= n {
+                table[j] = false;
+                j += j;
+            }
+        }
+        Self { table }
+    }
+
+    pub fn is_prime<T>(&self, n: T) -> bool
+    where
+        T: std::slice::SliceIndex<[bool], Output = bool>,
+    {
+        self.table[n]
+    }
+
+    pub fn prime_list<T: std::convert::From<usize>>(&self) -> Vec<T> {
+        self.table
+            .iter()
+            .enumerate()
+            .filter(|(_, &p)| p)
+            .map(|(i, _)| i.into())
+            .collect()
+    }
+}
+
+pub fn make_prime_list(n: usize) -> Vec<usize> {
+    let mut prime_list = Vec::with_capacity(n / 4);
+    for i in 2..=n {
+        let mut ok = true;
+        for &p in &prime_list {
+            if i < p * p {
+                break;
+            }
+            if i % p == 0 {
+                ok = false;
+                break;
+            }
+        }
+        if ok {
+            prime_list.push(i);
+        }
+    }
+    prime_list
+}
